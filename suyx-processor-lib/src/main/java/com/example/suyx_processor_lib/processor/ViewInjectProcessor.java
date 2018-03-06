343cc3ea2277653630b54f6cede3c66866927ee2
suyxin <suyongxin@chooseway.com>
2018-03-06 22:37:19 +0800

    JAVAPopet的使用

--- suyx-processor-lib/src/main/java/com/example/suyx_processor_lib/processor/ViewInjectProcessor.java
+++ suyx-processor-lib/src/main/java/com/example/suyx_processor_lib/processor/ViewInjectProcessor.java
@@ -3,9 +3,11 @@ package com.example.suyx_processor_lib.processor;
 import com.example.suyx_annotation_lib.annotation.Hello;
 import com.example.suyx_annotation_lib.annotation.ViewInject;
 import com.google.auto.service.AutoService;
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.TypeName;
+import com.squareup.javapoet.TypeSpec;
 
-import java.io.IOException;
-import java.io.Writer;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashSet;
@@ -16,9 +18,9 @@ import java.util.Set;
 import javax.annotation.processing.Processor;
 import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.element.Element;
+import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.element.VariableElement;
-import javax.tools.JavaFileObject;
 
 /**
  * Created by suyxin on 2018/3/3.
@@ -77,6 +79,41 @@ public class ViewInjectProcessor extends BaseProcessor {
             String PACKAGE = fullName.replace("." + simpleName, "");
             String newSimpleClassName = entry.getValue().getSimpleName() + SUFFIX;
 
+//            StringBuilder builder = new StringBuilder()
+//                    .append("package " + PACKAGE + ";\n\n")
+//                    .append("public class ")
+//                    .append(newSimpleClassName)
+//                    .append(" {\n\n");
+//
+//            String constorStr = "\tpublic %s(%s activity) {\n\n";
+//            builder.append(String.format(constorStr, newSimpleClassName, fullName));// open method
+//
+//            String findViewByIdStr = "\t\tactivity.%s = (%s)activity.findViewById(%s);\n";
+//            for (VariableInfo info : classMap.get(entry.getKey())) {
+//
+//                builder.append(String.format(findViewByIdStr, info.variableElement.getSimpleName(), info.variableElement.asType().toString(), info.viewId));
+//
+//            }
+//
+//
+//            builder.append("\t}\n") // close method
+//                    .append("}\n"); // close class
+//
+//
+//            try { // write the file
+//                JavaFileObject source = mFiler.createSourceFile(PACKAGE + "." + newSimpleClassName);
+//                Writer writer = source.openWriter();
+//                writer.write(builder.toString());
+//                writer.flush();
+//                writer.close();
+//            } catch (IOException e) {
+//                // Note: calling e.printStackTrace() will print IO errors
+//                // that occur from the file already existing after its first run, this is normal
+//            }
+
+
+
+
             /**
              * package com.example.suyxin.suyxprocessor;
 
@@ -92,40 +129,40 @@ public class ViewInjectProcessor extends BaseProcessor {
              }
              }
              */
-            StringBuilder builder = new StringBuilder()
-                    .append("package " + PACKAGE + ";\n\n")
-                    .append("public class ")
-                    .append(newSimpleClassName)
-                    .append(" {\n\n");
 
-            String constorStr = "\tpublic %s(%s activity) {\n\n";
-            builder.append(String.format(constorStr, newSimpleClassName, fullName));// open method
+            try {
+                MethodSpec.Builder constructorBuilder = MethodSpec.constructorBuilder()
+                        .addModifiers(Modifier.PUBLIC)
+                        .addParameter(TypeName.get(typeElement.asType()), "activity");
 
-            String findViewByIdStr = "\t\tactivity.%s = (%s)activity.findViewById(%s);\n";
-            for (VariableInfo info : classMap.get(entry.getKey())) {
+                String findViewByIdStr = "activity.%s = (%s)activity.findViewById(%s)";
 
-                builder.append(String.format(findViewByIdStr, info.variableElement.getSimpleName(), info.variableElement.asType().toString(), info.viewId));
+                for (VariableInfo info : classMap.get(entry.getKey())) {
 
+                    String statement =  String.format(findViewByIdStr, info.variableElement.getSimpleName(), info.variableElement.asType().toString(), info.viewId);
+                    constructorBuilder.addStatement(statement);
+
+                }
+                MethodSpec constructorMethod = constructorBuilder.build();
+                TypeSpec typeSpec = TypeSpec.classBuilder(newSimpleClassName)
+                        .addModifiers(Modifier.PUBLIC)
+                        .addMethod(constructorMethod)
+                        .build();
+
+                JavaFile javaFile = JavaFile.builder(PACKAGE, typeSpec)
+                        .build();
+                javaFile.writeTo(mFiler);
+
+            } catch (Exception e) {
+                e.printStackTrace();
             }
 
 
-            builder.append("\t}\n") // close method
-                    .append("}\n"); // close class
 
 
-            try { // write the file
-                JavaFileObject source = mFiler.createSourceFile(PACKAGE + "." + newSimpleClassName);
-                Writer writer = source.openWriter();
-                writer.write(builder.toString());
-                writer.flush();
-                writer.close();
-            } catch (IOException e) {
-                // Note: calling e.printStackTrace() will print IO errors
-                // that occur from the file already existing after its first run, this is normal
-            }
+
 
             info(">>> findViewById is finish... <<<");
-
 
         }
 
